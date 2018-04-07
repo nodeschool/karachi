8e3d96c64a96f1f4f64553c39f3cb8f60714cbf2
AdeelHussain <mr.adi180@gmail.com>
2018-04-07 23:00:57 +0500

    Implemented API for saving and getting users data

--- events/meetup-2__rest-apis-workshop/workshop-content/2-parsing-json-payload/routes/users.js
+++ events/meetup-2__rest-apis-workshop/workshop-content/2-parsing-json-payload/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  
+  User.find()
+  .then((savedUsers) => {
+    res.send(savedUsers);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).send(err);
+
+  })
+});
+
+router.post('/', function (req, res, next) {
+  console.log('DAta------', req.body)
+  var user = new User(req.body);
+  user.save()
+    .then((savedUser) => {
+      res.send(savedUser);
+
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send(err);
+    })
 });
 
 module.exports = router;
